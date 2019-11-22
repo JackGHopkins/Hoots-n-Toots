4740fda256b90821109234cba7e1b27706eb688d
JackGHopkins <jackghopkins@gmail.com>
2019-11-22 00:34:43 +0000

    Got placement of tokens down. Next need to make Token object.

--- Hoots_n_Toots/game.cpp
+++ Hoots_n_Toots/game.cpp
@@ -1,3 +1,8 @@
+//game.cpp
+
+#include <string>
+using namespace std;
+
 #define is_down(b) input->buttons[b].is_down
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define release(b) (!input->buttons[b].is_down && input->buttons[b].changed)
@@ -8,91 +13,115 @@ float gridline_width = 0.2f;
 float gridline_length = 24.f;
 float gridline_length_mod = 0.2f;
 
-int gridline_spacing = 12.5;
+int grid_spacing = 12.5;
+
+int token_mod = 18;
 
 // Selecting Square in grid
 int pos_x = 0;
 int pos_y = 0;
 
-int player_colour = 0;
+int player_colour = 1;
+int temp_colour = 1;
 
-int grid_select[4][4] = {
+int grid[4][4] = {
    {0, 0, 0, 0} ,   /*  initialisers for row indexed by 0 */
    {0, 0, 0, 0} ,   /*  initialisers for row indexed by 1 */
    {0, 0, 0, 0} ,	/*  initialisers for row indexed by 2 */
    {0, 0, 0, 0}		/*  initialisers for row indexed by 3 */
 };
 
+
 int win_condition[24] = { {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, };
 
-
-class Token {
-private:
-	int m_width;
-	int m_height;
-	int m_pos_x;
-	int m_pos_y;
-	string m_colour;
-public:
-	Token() {}
-	Token(const )
-};
-
 internal void
-simulate_game (Input* input, float dt) {
+simulate_game(Input* input, float dt) {
 	clear_screen(0x000000);
 
-	if (player_colour == 0) {
-		draw_rect(player_pos_x, player_pos_y, 5, 5, 0xff0000);
-	}
 
-	if (player_colour == 1) {
-		draw_rect(player_pos_x, player_pos_y, 5, 5, 0x0000ff);
-	}
-
+	// Selector Controls
 	if (pressed(BUTTON_UP) && pos_y > 0) {
 		pos_y = pos_y - 1;
-		player_pos_y = player_pos_y + gridline_spacing;
+		player_pos_y = player_pos_y + grid_spacing;
 	}
 
 	if (pressed(BUTTON_DOWN) && pos_y < 3) {
 		pos_y = pos_y + 1;
-		player_pos_y = player_pos_y - gridline_spacing;
+		player_pos_y = player_pos_y - grid_spacing;
 	}
 
 	if (pressed(BUTTON_LEFT) && pos_x > 0) {
 		pos_x = pos_x - 1;
-		player_pos_x = player_pos_x - gridline_spacing;
+		player_pos_x = player_pos_x - grid_spacing;
 	}
 
 	if (pressed(BUTTON_RIGHT) && pos_x < 3) {
 		pos_x = pos_x + 1;
-		player_pos_x = player_pos_x + gridline_spacing;
+		player_pos_x = player_pos_x + grid_spacing;
 	}
 
 
 	// Placing tokens for - 
-	if (pressed(BUTTON_LMB) && player_colour == 0) {
-		draw_rect(player_pos_x, player_pos_y, 4, 2, 0xff0000);
-		
+	if (pressed(BUTTON_LMB) && player_colour == 1 && temp_colour == 1) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				// Check if spot is empty.
+				if (grid[i][j] != -1) {
+					grid[pos_y][pos_x] = 1;
+					player_colour = -1;
+					temp_colour = 1;
+				}
+			}
+		}
 	}
 
-	if (pressed(BUTTON_LMB) && player_colour == 1) {
-		draw_rect(player_pos_x, player_pos_y, 2, 4, 0x0000ff);
-		player_colour = 0;
+	if (pressed(BUTTON_LMB) && player_colour == -1 && temp_colour == -1 ) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				// Check if spot is empty.
+				if (grid[i][j] != 1) {
+					grid[pos_y][pos_x] = -1;
+					player_colour = 1;
+					temp_colour = -1;
+				}
+			}
+		}
+	}
+
+	// Colour Switches
+	if (temp_colour == 1) {
+		temp_colour = -1;
+	}
+	else {
+		temp_colour = 1;
+	}
+
+	//Selector Colour.
+	if (player_colour == 1) {
+		draw_rect(player_pos_x, player_pos_y, 5, 5, 0xff0000);
+	}
+	if (player_colour == -1) {
+		draw_rect(player_pos_x, player_pos_y, 5, 5, 0x0000ff);
 	}
 
 	// Horizontal Lines.
-	draw_rect(0, 2*gridline_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
-	draw_rect(0, gridline_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
+	draw_rect(0, 2 * grid_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
+	draw_rect(0, grid_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
 	draw_rect(0, 0, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
-	draw_rect(0, -1*gridline_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
-	draw_rect(0, -2*gridline_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
-	
+	draw_rect(0, -1 * grid_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
+	draw_rect(0, -2 * grid_spacing, gridline_length + gridline_length_mod, gridline_width, 0xffffff);
+
 	// Vertical Lines.
-	draw_rect(2 * gridline_spacing, 0, gridline_width, gridline_length, 0xffffff);
-	draw_rect(gridline_spacing, 0, gridline_width, gridline_length, 0xffffff);
+	draw_rect(2 * grid_spacing, 0, gridline_width, gridline_length, 0xffffff);
+	draw_rect(grid_spacing, 0, gridline_width, gridline_length, 0xffffff);
 	draw_rect(0, 0, gridline_width, gridline_length, 0xffffff);
-	draw_rect(-1 * gridline_spacing, 0, gridline_width, gridline_length, 0xffffff);
-	draw_rect(-2 * gridline_spacing, 0, gridline_width, gridline_length, 0xffffff);
+	draw_rect(-1 * grid_spacing, 0, gridline_width, gridline_length, 0xffffff);
+	draw_rect(-2 * grid_spacing, 0, gridline_width, gridline_length, 0xffffff);
+
+	// Draw Tokens.
+	draw_token(grid, pos_x, pos_y, grid_spacing, token_mod);
+
+	check_win(win_condition, grid);
 }
--- Hoots_n_Toots/renderer.cpp
+++ Hoots_n_Toots/renderer.cpp
@@ -1,6 +1,8 @@
+// renderer.cpp
+
 global_variable float render_scale = 0.01f;
 
-internal void 
+internal void
 clear_screen(u32 color) {
 	unsigned int* pixel = (u32*)render_state.memory;
 
@@ -11,9 +13,9 @@ clear_screen(u32 color) {
 	}
 }
 
-internal void 
+internal void
 draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
-	
+
 	x0 = clamp(0, x0, render_state.width);
 	x1 = clamp(0, x1, render_state.width);
 	y0 = clamp(0, y0, render_state.height);
@@ -21,7 +23,7 @@ draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
 
 
 	for (int y = y0; y < y1; y++) {
-		unsigned int* pixel = (u32*)render_state.memory + x0 + y*render_state.width;
+		unsigned int* pixel = (u32*)render_state.memory + x0 + y * render_state.width;
 		for (int x = x0; x < x1; x++) {
 			*pixel++ = color;
 		}
@@ -48,4 +50,141 @@ draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
 
 
 	draw_rect_in_pixels(x0, y0, x1, y1, color);
+}
+
+internal void
+draw_token(int grid[4][4], int pos_x, int pos_y, int grid_spacing, int token_mod) {
+
+	// Iterate through all squares on the board to check if a token has been placed.
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (grid[i][j] == 1 ) {
+				draw_rect(j * grid_spacing - token_mod, i * -grid_spacing + token_mod, 4, 1, 0xff0000);
+			}
+			else if (grid[i][j] == -1 )
+				draw_rect(j * grid_spacing - token_mod, i * -grid_spacing + token_mod, 1, 4, 0x0000ff);
+		}
+
+	}
+}
+
+
+bool x;
+bool o;
+
+void check_win(int win_condition[24], int grid[4][4]) {
+
+	// 24 * 2 Conditions in total.
+	int i;
+	x = true;
+
+	if (x == true) {
+		i = 1;
+	}
+	else {
+		i = -1;
+	}
+
+	// Horizontal Row wins
+	if (grid[0][0] || grid[0][1] || grid[0][2]) {
+		win_condition[0] = win_condition[0] + i;
+	}
+
+	if (grid[0][1] || grid[0][2] || grid[0][3]) {
+		win_condition[1] = win_condition[1] + i;
+	}
+
+	if (grid[1][0] || grid[1][1] || grid[1][2]) {
+		win_condition[2] = win_condition[2] + i;
+	}
+
+	if (grid[1][1] || grid[1][2] || grid[1][3]) {
+		win_condition[3] = win_condition[3] + i;
+	}
+
+	if (grid[2][0] || grid[2][1] || grid[2][2]) {
+		win_condition[4] = win_condition[4] + i;
+	}
+
+	if (grid[2][1] || grid[2][2] || grid[2][3]) {
+		win_condition[5] = win_condition[5] + i;
+	}
+
+	if (grid[3][0] || grid[3][1] || grid[3][2]) {
+		win_condition[6] = win_condition[6] + i;
+	}
+
+	if (grid[3][1] || grid[3][2] || grid[3][3]) {
+		win_condition[7] = win_condition[7] + i;
+	}
+
+
+	// Vertical Row Wins
+	if (grid[0][0] || grid[1][0] || grid[2][0]) {
+		win_condition[8] = win_condition[8] + i;
+	}
+
+	if (grid[1][0] || grid[2][0] || grid[3][0]) {
+		win_condition[9] = win_condition[9] + i;
+	}
+
+	if (grid[0][1] || grid[1][1] || grid[2][1]) {
+		win_condition[10] = win_condition[10] + i;
+	}
+
+	if (grid[1][1] || grid[2][1] || grid[3][1]) {
+		win_condition[11] = win_condition[11] + i;
+	}
+
+	if (grid[0][2] || grid[1][2] || grid[2][2]) {
+		win_condition[12] = win_condition[12] + i;
+	}
+
+	if (grid[1][2] || grid[2][2] || grid[3][2]) {
+		win_condition[13] = win_condition[13] + i;
+	}
+
+	if (grid[0][3] || grid[1][3] || grid[2][3]) {
+		win_condition[14] = win_condition[14] + i;
+	}
+
+	if (grid[1][3] || grid[2][3] || grid[3][3]) {
+		win_condition[15] = win_condition[15] + i;
+	}
+
+	// Diagonal Forward Row Win
+
+	if (grid[0][0] || grid[1][1] || grid[2][2]) {
+		win_condition[16] = win_condition[16] + i;
+	}
+
+	if (grid[0][1] || grid[1][2] || grid[2][3]) {
+		win_condition[17] = win_condition[17] + i;
+	}
+
+	if (grid[1][0] || grid[2][1] || grid[3][2]) {
+		win_condition[18] = win_condition[18] + i;
+	}
+
+	if (grid[1][1] || grid[2][2] || grid[3][3]) {
+		win_condition[19] = win_condition[19] + i;
+	}
+
+	// Diagonal Backwards Row Win
+
+	if (grid[0][2] || grid[1][1] || grid[2][0]) {
+		win_condition[20] = win_condition[20] + i;
+	}
+
+	if (grid[1][2] || grid[2][1] || grid[3][0]) {
+		win_condition[21] = win_condition[21] + i;
+	}
+
+	if (grid[0][3] || grid[1][2] || grid[2][1]) {
+		win_condition[22] = win_condition[22] + i;
+	}
+
+	if (grid[1][3] || grid[2][2] || grid[3][1]) {
+		win_condition[23] = win_condition[23] + i;
+	}
 }
